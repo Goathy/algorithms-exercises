471e107e00066dd26b07f19203830832bcf15844
Jakub Gałecki <goathy.dev@gmail.com>
2023-12-05 11:40:00 +0100

    implement avl tree

--- specs/avl/avl.test.js
+++ specs/avl/avl.test.js
@@ -22,51 +22,163 @@
 */
 
 class Tree {
-  // code goes here
+	#root
+
+	constructor() {
+		this.#root = null
+	}
+
+	add(value) {
+		if (this.#root === null) {
+			this.#root = new Node(value) 
+		} else {
+			this.#root.add(value)
+		}
+	}
+
+	toObject() {
+		return this.#root
+	}
 }
 
 class Node {
-  // code also goes here
+	constructor(value) {
+		this.value = value
+		this.left = null
+		this.right = null
+		this.height = 1
+	}
+
+	add(value) {
+		if (this.value > value) {
+			if (this.left === null) {
+				this.left = new Node(value)
+			} else {
+				this.left.add(value)
+			}
+			if (this.right === null || this.right.height < this.left.height) {
+				this.height = this.left.height + 1
+			}
+
+		} else {
+			if (this.right === null) {
+				this.right = new Node(value)
+			} else {
+				this.right.add(value)
+			}
+
+			if (this.left === null || this.left.height < this.right.height) {
+				this.height = this.right.height + 1
+			}
+		}
+
+		this.balance()
+	}
+
+
+	balance() {
+		const rightHeight = this.right?.height || 0
+		const leftHeight = this.left?.height || 0
+
+		if (leftHeight > rightHeight + 1) {
+			const leftRightHeight = this.left.right?.height || 0
+			const leftLeftHeight = this.left.left?.height || 0
+
+			if (leftRightHeight > leftLeftHeight) {
+				this.left.rotateRR()
+			}
+
+			this.rotateLL()
+		}
+
+		if (rightHeight > leftHeight + 1) {
+			const rightLeftHeight = this.right.left?.height || 0
+			const rightRightHeight = this.right.right?.height || 0
+
+			if (rightLeftHeight > rightRightHeight) {
+				this.right.rotateLL()
+			}
+
+			this.rotateRR()
+		}
+	}
+
+	rotateRR() {
+		const valueBefore = this.value
+		const leftBefore = this.left
+		this.value = this.right.value
+		this.left = this.right
+		this.right = this.right.right
+		this.left.right = this.left.left
+		this.left.left = leftBefore
+		this.left.value = valueBefore
+		this.left.updateInNewLocation()
+		this.updateInNewLocation()
+	}
+
+	rotateLL() {
+		const valueBefore = this.value
+		const rightBefore = this.right
+		this.value = this.left.value
+		this.right = this.left
+		this.left = this.left.left
+		this.right.left = this.right.right
+		this.right.right = rightBefore
+		this.right.value = valueBefore
+		this.right.updateInNewLocation()
+		this.updateInNewLocation()
+	}
+
+	updateInNewLocation() {
+		if (this.right === null && this.left === null) {
+			this.height = 1
+		} else if (this.right === null || (this.left !== null && this.right.height < this.left.height)) {
+			this.height = this.left.height + 1
+		} else {
+			this.height = this.right.height + 1
+		}
+
+	}
 }
 
 // unit tests
 // do not modify the below code
-describe.skip("AVL Tree", function () {
-  test("creates a correct tree", () => {
-    const nums = [3, 7, 4, 6, 5, 1, 10, 2, 9, 8];
-    const tree = new Tree();
-    nums.map((num) => tree.add(num));
-    const objs = tree.toObject();
+describe("AVL Tree", function () {
+	test("creates a correct tree", () => {
+		const nums = [3, 7, 4, 6, 5, 1, 10, 2, 9, 8];
+		const tree = new Tree();
+		nums.map((num) => tree.add(num));
+		const objs = tree.toObject();
 
-    expect(objs.value).toEqual(4);
+		expect(objs.value).toEqual(4);
 
-    expect(objs.left.value).toEqual(2);
+		expect(objs.left.value).toEqual(2);
 
-    expect(objs.left.left.value).toEqual(1);
-    expect(objs.left.left.left).toBeNull();
-    expect(objs.left.left.right).toBeNull();
+		expect(objs.left.left.value).toEqual(1);
+		expect(objs.left.left.left).toBeNull();
+		expect(objs.left.left.right).toBeNull();
 
-    expect(objs.left.right.value).toEqual(3);
-    expect(objs.left.right.left).toBeNull();
-    expect(objs.left.right.right).toBeNull();
+		expect(objs.left.right.value).toEqual(3);
+		expect(objs.left.right.left).toBeNull();
+		expect(objs.left.right.right).toBeNull();
 
-    expect(objs.right.value).toEqual(7);
+		expect(objs.right.value).toEqual(7);
 
-    expect(objs.right.left.value).toEqual(6);
-    expect(objs.right.left.right).toBeNull();
+		expect(objs.right.left.value).toEqual(6);
+		expect(objs.right.left.right).toBeNull();
 
-    expect(objs.right.left.left.value).toEqual(5);
-    expect(objs.right.left.left.left).toBeNull();
-    expect(objs.right.left.left.right).toBeNull();
+		expect(objs.right.left.left.value).toEqual(5);
+		expect(objs.right.left.left.left).toBeNull();
+		expect(objs.right.left.left.right).toBeNull();
 
-    expect(objs.right.right.value).toEqual(9);
+		expect(objs.right.right.value).toEqual(9);
 
-    expect(objs.right.right.left.value).toEqual(8);
-    expect(objs.right.right.left.left).toBeNull();
-    expect(objs.right.right.left.right).toBeNull();
+		expect(objs.right.right.left.value).toEqual(8);
+		expect(objs.right.right.left.left).toBeNull();
+		expect(objs.right.right.left.right).toBeNull();
 
-    expect(objs.right.right.right.value).toEqual(10);
-    expect(objs.right.right.right.left).toBeNull();
-    expect(objs.right.right.right.right).toBeNull();
-  });
+		expect(objs.right.right.right.value).toEqual(10);
+		expect(objs.right.right.right.left).toBeNull();
+		expect(objs.right.right.right.right).toBeNull();
+	});
 });
