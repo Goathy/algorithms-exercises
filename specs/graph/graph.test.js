ce9270a04ad452a8a9a5baa2e5cb0a6b99377dc1
Jakub Gałecki <goathy.dev@gmail.com>
2023-12-06 18:47:05 +0100

    implement graph traversal algorithm

--- specs/graph/graph.test.js
+++ specs/graph/graph.test.js
@@ -26,12 +26,50 @@
 const { getUser } = require("./jobs");
 
 const findMostCommonTitle = (myId, degreesOfSeparation) => {
-  // code goes here
+	const queue = [[myId, 0]]
+	const seen = new Set(queue)
+
+	const jobs = {}
+
+	while(queue.length) { 
+		const [uid, depth] = queue.shift()
+		const { title, connections } = getUser(uid)
+		if (jobs[title]) {
+			jobs[title]++ 
+		} else {
+			jobs[title] = 1
+		}
+
+		if (depth < degreesOfSeparation) {
+			for (let i = 0; i < connections.length; i++) {
+				if (seen.has(connections[i]) === false) {
+					queue.push([connections[i], depth + 1])
+					seen.add(connections[i])
+				}
+			}
+		} 
+	}
+
+	const jobList = Object.entries(jobs).map(([name, value]) => ({name, value}))
+
+	for (let i = 1; i < jobList.length; i++) {
+		for (let j = 0; j < i; j++) {
+			if (jobList[j].value > jobList[i].value) {
+				const tmp = jobList[j]
+				jobList[j] = jobList[i]
+				jobList[i] = tmp
+			}
+		}
+
+	}
+
+	return jobList[jobList.length - 1].name
 };
+
 
 // unit tests
 // do not modify the below code
-test.skip("findMostCommonTitle", function () {
+describe("findMostCommonTitle", function () {
   // the getUser function and data comes from this CodePen: https://codepen.io/btholt/pen/NXJGwa?editors=0010
   test("user 30 with 2 degrees of separation", () => {
     expect(findMostCommonTitle(30, 2)).toBe("Librarian");
@@ -48,7 +86,7 @@ test.skip("findMostCommonTitle", function () {
   });
 });
 
-test.skip("extra credit", function () {
+describe("extra credit", function () {
   test("user 1 with 7 degrees of separation – this will traverse every user that's followed by someone else. five users are unfollowed", () => {
     expect(findMostCommonTitle(1, 7)).toBe("Geological Engineer");
   });
