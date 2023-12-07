8e0e4b93808a43dbab9f1dc92d2237e22fc504a6
Jakub Gałecki <goathy.dev@gmail.com>
2023-12-08 00:56:53 +0100

    implement path finding algorithm

--- specs/pathfinding/pathfinding.test.js
+++ specs/pathfinding/pathfinding.test.js
@@ -16,17 +16,124 @@
 // the way I did. however feel free to use it if you'd like
 const logMaze = require("./logger");
 
+const WALL = 1
+const A = 'a'
+const B = 'b'
+const NO_ONE = 'no_one'
+
 function findShortestPathLength(maze, [xA, yA], [xB, yB]) {
-  // code goes here
+	const grid = createGrid(maze)
+	grid[yA][xA].openedBy = A
+	grid[yB][xB].openedBy = B
+
+	const aQueue = [
+		grid[yA][xA]
+	]
+
+	const bQueue = [
+		grid[yB][xB]
+	]
+
+	let steps = 0
+	while(aQueue.length && bQueue.length) {
+		steps++
+
+		const aChildrens = getChildrens(grid, aQueue)
+
+		for (const children of aChildrens) {
+			if (children.openedBy === B) {
+				return children.distance + steps
+			}
+
+			if (children.openedBy === NO_ONE) {
+				children.openedBy = A
+				children.distance = steps
+				aQueue.push(children)
+			}
+		}
+
+		const bChildrens = getChildrens(grid, bQueue)
+
+		for (const children of bChildrens) {
+			if (children.openedBy === A) {
+				return children.distance + steps
+			}
+
+			if (children.openedBy === NO_ONE) {
+				children.openedBy = B
+				children.distance = steps
+				bQueue.push(children)
+			}
+		}
+	}
+
+	return -1
 }
 
+function createGrid(maze) {
+	const grid = []  
+	for (let y = 0; y < maze.length; y++) { 
+		const row = []
+
+		for (let x = 0; x < maze[y].length; x++) {
+			row[x] = {
+				x, y,
+				closed: maze[y][x] === WALL,
+				distance: 0,
+				openedBy: NO_ONE
+			}
+		}
+
+		grid[y] = row
+	}
+
+	return grid
+}
+
+function getChildrens(grid, queue) {
+	let childrens = []
+	while(queue.length) {
+		const node = queue.shift()
+
+		const neighbours = getNeighbours(grid, node)
+		childrens = childrens.concat(neighbours)
+	}
+
+	return childrens
+}
+
+function getNeighbours(grid, { x, y }) {
+	const childrens = []
+
+	// up
+	if (x - 1 >= 0 && grid[y][x - 1].closed === false) {
+		childrens.push(grid[y][x - 1])
+	}
+
+	// down
+	if (x + 1 < grid.length && grid[y][x + 1].closed === false) {
+		childrens.push(grid[y][x + 1])
+	}
+
+	// left
+	if (y - 1 >= 0 && grid[y - 1][x].closed === false) {
+		childrens.push(grid[y - 1][x])
+	}
+
+	// right
+	if (y + 1 < grid[y].length && grid[y + 1][x].closed === false) {
+		childrens.push(grid[y + 1][x])
+	}
+
+	return childrens
+}
 // there is a visualization tool in the completed exercise
 // it requires you to shape your objects like I did
 // see the notes there if you want to use it
 
 // unit tests
 // do not modify the below code
-describe.skip("pathfinding – happy path", function () {
+describe("pathfinding – happy path", function () {
   const fourByFour = [
     [2, 0, 0, 0],
     [0, 0, 0, 0],
@@ -90,7 +197,7 @@ describe.skip("pathfinding – happy path", function () {
 // I care far less if you solve these
 // nonetheless, if you're having fun, solve some of the edge cases too!
 // just remove the .skip from describe.skip
-describe.skip("pathfinding – edge cases", function () {
+describe("pathfinding – edge cases", function () {
   const byEachOther = [
     [0, 0, 0, 0, 0],
     [0, 2, 2, 0, 0],
